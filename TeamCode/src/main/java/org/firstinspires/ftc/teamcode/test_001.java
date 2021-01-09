b28cc0d0c1900c64228646f46e0cb4313f1bd1c9
georgerapeanu <alexandrurapeanu@yahoo.com>
2021-01-09 10:21:10 +0200

    one last fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_001.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_001.java
@@ -34,7 +34,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-import org.firstinspires.ftc.teamcode.RemoteDrive;
 
 
 /**
